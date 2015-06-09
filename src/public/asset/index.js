da4f2d811c151370bf903ff1bb82c6f3b9f87c5b
hendratjua@gmail.com <hendratjua@gmail.com>
2015-06-09 14:29:04 +0700

    remove static token

--- src/public/asset/index.js
+++ src/public/asset/index.js
@@ -362,74 +362,71 @@ app.controller('LoginController', ['$rootScope', '$scope', '$http', '$location',
 
             $scope.login.working = true;
 
-            CookieStore.setData( 'nvWAuuwj0qt0sN7L9ojbimM4p5yagEc2aH1O2AuSsHCrQ11XDS8SQ82rGcysy40nqOmcf4gSpANwvXLbzPrlDZBxdQVepm7J9YGPnybVZ0k9lMYffGtw1cycTMzE0XlhxW8Ihs22KHbaUsl95vk3eWG1ybCsAvsSiyedcMGepsg1QC2TgwEdz64MJHurZXCejaipnJDGspVVaZcNgUaxypAl5thYpgYpQVZyzeO9oMD7mAQ0lQwW671UxCHY5gLd', '556d00e517aac10c2bbcaa8d' );
-            $location.path( '/' );
+            var auth = base64_encode( globalConfig.client_id+':'+globalConfig.client_secret );
+            var grant_type = encodeURIComponent( globalConfig.grant_type );
+            var uri = auth_url+'oauth2/token';
+            var send = {
+                grant_type: grant_type,
+                username: username,
+                password: password
+            };
 
-//            var auth = base64_encode( globalConfig.client_id+':'+globalConfig.client_secret );
-//            var grant_type = encodeURIComponent( globalConfig.grant_type );
-//            var uri = auth_url+'oauth2/token';
-//            var send = {
-//                grant_type: grant_type,
-//                username: username,
-//                password: password
-//            };
-//
-//            $http.post( uri , $.param(send), {
-//                headers: {
-//                    'Authorization': 'Basic '+auth
-//                }
-//            })
-//                .success(function(response) {
-//
-//                    $http.get( api_url+'organizations' , {
-//                        headers: {
-//                            'Authorization': 'Bearer '+response.access_token
-//                        }
-//                    })
-//                        .success(function(responseClient) {
-//
-//                            var get_hosting_name = $location.host();
-//                            var grand_access = false;
-//                            var get_id = false;
-//
-//                            if(responseClient.success == true && responseClient.total > 0)
-//                            {
-//                                for(var i=0; i<responseClient.total; i++)
-//                                {
-//                                    if(get_hosting_name == responseClient.data[i].url)
-//                                    {
-//                                        grand_access = true;
-//                                        get_id = responseClient.data[i]._id;
-//                                    }
-//                                }
-//                            }
-//
-//                            if(grand_access)
-//                            {
-//                                CookieStore.setData( response.access_token, get_id );
-//                                $location.path( '/' );
-//                            }
-//                            else
-//                            {
-//                                showError("You don't have any permission to access this page", 1);
-//                                $scope.login.working = false;
-//                            }
-//
-//                        })
-//                        .error(function(responseClient) {
-//
-//                            showError(responseClient, 1);
-//                            $scope.login.working = false;
-//
-//                        });
-//
-//                })
-//                .error(function(response) {
-//
-//                    showError(response, 1);
-//                    $scope.login.working = false;
-//
-//                });
+            $http.post( uri , $.param(send), {
+                headers: {
+                    'Authorization': 'Basic '+auth
+                }
+            })
+                .success(function(response) {
+
+                    $http.get( api_url+'organizations' , {
+                        headers: {
+                            'Authorization': 'Bearer '+response.access_token
+                        }
+                    })
+                        .success(function(responseClient) {
+
+                            var get_hosting_name = $location.host();
+                            var grand_access = false;
+                            var get_id = false;
+
+                            if(responseClient.success == true && responseClient.total > 0)
+                            {
+                                for(var i=0; i<responseClient.total; i++)
+                                {
+                                    if(get_hosting_name == responseClient.data[i].url)
+                                    {
+                                        grand_access = true;
+                                        get_id = responseClient.data[i]._id;
+                                    }
+                                }
+                            }
+
+                            if(grand_access)
+                            {
+                                CookieStore.setData( response.access_token, get_id );
+                                $location.path( '/' );
+                            }
+                            else
+                            {
+                                showError("You don't have any permission to access this page", 1);
+                                $scope.login.working = false;
+                            }
+
+                        })
+                        .error(function(responseClient) {
+
+                            showError(responseClient, 1);
+                            $scope.login.working = false;
+
+                        });
+
+                })
+                .error(function(response) {
+
+                    showError(response, 1);
+                    $scope.login.working = false;
+
+                });
 
         }
 
