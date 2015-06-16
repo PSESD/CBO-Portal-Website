2ad5f2a16d482d18d2f5b0e3b83212ef9f9cf5cd
hendratjua@gmail.com <hendratjua@gmail.com>
2015-06-16 20:40:59 +0700

    remove demo test

--- src/public/asset/index.js
+++ src/public/asset/index.js
@@ -848,79 +848,76 @@ app.controller('LoginController', ['$rootScope', '$scope', '$http', '$location',
 
         $scope.loginMe = function(username, password) {
 
-            CookieStore.setData( 'DQwFbJr34LaDOI5toUKfN6C1MDBn0UxE1xc3YkQwJpEhoEb25f7clJ6b6e1iAuOXXqA7KzBv096GJGDWlili8rPRC5SYbqYNQY9m51IzfEwgjSR88w3RfxIe8ROrquWhT2ET9PvaxDatOF4NgWGnqCKMCgO2EDZK9xZFfeG25YqluO20qBLHoomkFgBciHXDMjEF80Z9PGeB0aqCrcURXpydT9R1ALvMZNx7yAgIOWzjT2F8PS8ilPYmCa5UpLYc', '556d00e517aac10c2bbcaa8d', 'helpinghand.cbo.upward.st' );
-            $location.path( '/' );
-//
-//            $scope.login.working = true;
-//
-//            var auth = base64_encode( globalConfig.client_id+':'+globalConfig.client_secret );
-//            var grant_type = encodeURIComponent( globalConfig.grant_type );
-//            var uri = auth_url+'oauth2/token';
-//            var send = {
-//                grant_type: grant_type,
-//                username: username,
-//                password: password
-//            };
-//
-//            $http.post( uri , $.param(send), {
-//                headers: {
-//                    'Authorization': 'Basic '+auth
-//                }
-//            })
-//                .success(function(response) {
-//
-//                    $http.get( api_url+'organizations' , {
-//                        headers: {
-//                            'Authorization': 'Bearer '+response.access_token
-//                        }
-//                    })
-//                        .success(function(responseClient) {
-//
-//                            var get_hosting_name = $location.host();
-//                            var grand_access = false;
-//                            var get_id = false;
-//                            var get_redirect_url = false;
-//
-//                            if(responseClient.success == true && responseClient.total > 0)
-//                            {
-//                                for(var i=0; i<responseClient.total; i++)
-//                                {
-//                                    console.log(responseClient.data[i].url);
-//                                    if(get_hosting_name == responseClient.data[i].url)
-//                                    {
-//                                        grand_access = true;
-//                                        get_id = responseClient.data[i]._id;
-//                                        get_redirect_url = responseClient.data[i].url;
-//                                    }
-//                                }
-//                            }
-//
-//                            if(grand_access)
-//                            {
-//                                CookieStore.setData( response.access_token, get_id, get_redirect_url );
-//                                $location.path( '/' );
-//                            }
-//                            else
-//                            {
-//                                showError("You don't have any permission to access this page", 1);
-//                                $scope.login.working = false;
-//                            }
-//
-//                        })
-//                        .error(function(responseClient) {
-//
-//                            showError(responseClient, 1);
-//                            $scope.login.working = false;
-//
-//                        });
-//
-//                })
-//                .error(function(response) {
-//
-//                    showError(response.error_description, 1);
-//                    $scope.login.working = false;
-//
-//                });
+            $scope.login.working = true;
+
+            var auth = base64_encode( globalConfig.client_id+':'+globalConfig.client_secret );
+            var grant_type = encodeURIComponent( globalConfig.grant_type );
+            var uri = auth_url+'oauth2/token';
+            var send = {
+                grant_type: grant_type,
+                username: username,
+                password: password
+            };
+
+            $http.post( uri , $.param(send), {
+                headers: {
+                    'Authorization': 'Basic '+auth
+                }
+            })
+                .success(function(response) {
+
+                    $http.get( api_url+'organizations' , {
+                        headers: {
+                            'Authorization': 'Bearer '+response.access_token
+                        }
+                    })
+                        .success(function(responseClient) {
+
+                            var get_hosting_name = $location.host();
+                            var grand_access = false;
+                            var get_id = false;
+                            var get_redirect_url = false;
+
+                            if(responseClient.success == true && responseClient.total > 0)
+                            {
+                                for(var i=0; i<responseClient.total; i++)
+                                {
+                                    console.log(responseClient.data[i].url);
+                                    if(get_hosting_name == responseClient.data[i].url)
+                                    {
+                                        grand_access = true;
+                                        get_id = responseClient.data[i]._id;
+                                        get_redirect_url = responseClient.data[i].url;
+                                    }
+                                }
+                            }
+
+                            if(grand_access)
+                            {
+                                CookieStore.setData( response.access_token, get_id, get_redirect_url );
+                                $location.path( '/' );
+                            }
+                            else
+                            {
+                                showError("You don't have any permission to access this page", 1);
+                                $scope.login.working = false;
+                            }
+
+                        })
+                        .error(function(responseClient) {
+
+                            showError(responseClient, 1);
+                            $scope.login.working = false;
+
+                        });
+
+                })
+                .error(function(response) {
+
+                    showError(response.error_description, 1);
+                    $scope.login.working = false;
+
+                });
 
         }
 
