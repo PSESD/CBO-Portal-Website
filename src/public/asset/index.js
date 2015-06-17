073064d11226b10d1bd905bcf64df4d2545c1c03
hendratjua@gmail.com <hendratjua@gmail.com>
2015-06-17 18:17:57 +0700

    for testing success in docker

--- src/public/asset/index.js
+++ src/public/asset/index.js
@@ -2,6 +2,7 @@
 
 var auth_url = "https://auth.cbo.upward.st/api/";
 var api_url = "https://api.cbo.upward.st/";
+
 var globalConfig = {
     client_id: 'cbo_client_demo',
     client_secret: '7e98a24f4fe91535348f6e87cde866dca9134b50fc029abefdc7278369f2',
