0ea5e06a4e65ba3a8e4a8de7fa0d1e92795e6a5e
hendratjua@gmail.com <hendratjua@gmail.com>
2015-07-01 01:13:34 +0700

    add edit student

--- src/public/asset/index.js
+++ src/public/asset/index.js
@@ -47,6 +47,11 @@ app.config(function ($routeProvider) {
             controller: 'StudentDetailController',
             access: { requiredAuthentication: true }
         }).
+        when('/student/edit/:student_id', {
+            templateUrl: 'asset/templates/student/edit.html',
+            controller: 'StudentEditController',
+            access: { requiredAuthentication: true }
+        }).
         when('/student/programs/:student_id/add', {
             templateUrl: 'asset/templates/student/program_add.html',
             controller: 'StudentProgramAddController',
@@ -386,6 +391,82 @@ app.controller('StudentBackpackController', ['$rootScope', '$scope', '$routePara
         var student_id = $routeParams.student_id;
 
         $http.get( api_url+AuthenticationService.organization_id+'/students/'+student_id+'/backpack', {
+            headers: {
+                'Authorization': 'Bearer '+AuthenticationService.token
+            }
+        })
+            .success(function(response) {
+
+                $scope.student = response;
+                $rootScope.doingResolve = false;
+
+            })
+            .error(function(response, status) {
+
+                console.log(response);
+                console.log(status);
+                showError(response, 1);
+                $rootScope.doingResolve = false;
+                if(status == 401)
+                {
+                    CookieStore.clearData();
+                    $location.path( '/login' );
+                }
+
+            });
+
+    }
+]);
+
+
+app.controller('StudentEditController', ['$rootScope', '$scope', '$routeParams', '$http', '$location', 'AuthenticationService', 'CookieStore',
+    function ($rootScope, $scope, $routeParams, $http, $location, AuthenticationService, CookieStore) {
+
+        $rootScope.full_screen = false;
+        $scope.student = {};
+
+        var student_id = $routeParams.student_id;
+
+        $scope.editStudent = function(student)
+        {
+            if(student)
+            {
+                $scope.working = true;
+                $http.put( api_url+AuthenticationService.organization_id+'/students/'+student_id, $.param(student), {
+                    headers: {
+                        'Authorization': 'Bearer '+AuthenticationService.token
+                    }
+                })
+                    .success(function(response) {
+
+                        if(response.success == true)
+                        {
+                            showError(response.message, 2);
+                        }
+                        else
+                        {
+                            showError(response.message, 1);
+                        }
+                        $scope.working = false;
+
+                    })
+                    .error(function(response, status) {
+
+                        console.log(response);
+                        console.log(status);
+                        showError(response, 1);
+                        $scope.working = false;
+                        if(status == 401)
+                        {
+                            CookieStore.clearData();
+                            $location.path( '/login' );
+                        }
+
+                    });
+            }
+        };
+
+        $http.get( api_url+AuthenticationService.organization_id+'/students/'+student_id, {
             headers: {
                 'Authorization': 'Bearer '+AuthenticationService.token
             }
