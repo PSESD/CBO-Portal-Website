99bd5ecdf49a8e1b487bc73f323a110ed0e16e05
ridwansidik85 <ridwan@upwardstech.com>
2015-08-05 17:10:37 +0700

    add config files for staging development and production

---
+++ src/config/staging/config.js
@@ -0,0 +1,11 @@
+'use strict';
+	
+var auth_url = "https://auth.sslstaging.studentsuccesslink.upward.st/api/";
+var api_url = "https://api.sslstaging.studentsuccesslink.upward.st.st/";
+
+var globalConfig = {
+    client_id: 'sslstaging.studentsuccesslink.upward.st',
+    client_secret: 'ed789e8077173fdaa3fa2430ffc0eec0aef43ac5519ee575aa8409a00195',
+    response_type: 'code',
+    grant_type: 'password' 
+};	
