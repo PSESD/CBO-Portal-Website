e621e1df9837ead24c0dbd64000c98fbff58bf20
hendratjua@gmail.com <hendratjua@gmail.com>
2016-01-20 01:44:42 +0700

    example 5

--- src/public/asset/controllers/ReportController.js
+++ src/public/asset/controllers/ReportController.js
@@ -10,7 +10,31 @@ app.controller('ReportController', ['$rootScope', '$scope', '$http', '$location'
         //report/students/race
 
         var colors = ["#7cb5ec", "#434348", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"];
-        var i;
+
+        $scope.programData = [];
+        $scope.districtData = [];
+        $scope.cohortData = [];
+        $scope.caseloadData = [];
+        $scope.total_student = 0;
+        $scope.total_school = 0;
+        $scope.total_user = 0;
+
+        $scope.programData.push("program 1");
+        $scope.programData.push("program 2");
+        $scope.programData.push("program 3");
+
+        $scope.districtData.push("district 1");
+        $scope.districtData.push("district 2");
+        $scope.districtData.push("district 3");
+
+        $scope.cohortData.push("cohort 1");
+        $scope.cohortData.push("cohort 2");
+        $scope.cohortData.push("cohort 3");
+
+        $scope.caseloadData.push("case load 1");
+        $scope.caseloadData.push("case load 2");
+        $scope.caseloadData.push("case load 3");
+
         $rootScope.doingResolve = false;
 
         $http.get(api_url + AuthenticationService.organization_id + '/report/students/school_district', {
@@ -22,6 +46,7 @@ app.controller('ReportController', ['$rootScope', '$scope', '$http', '$location'
 
                 var temp_districts = [];
                 var temp_studentSchools = [];
+                $scope.total_school = 0;
 
                 for (var index in response)
                 {
@@ -40,6 +65,8 @@ app.controller('ReportController', ['$rootScope', '$scope', '$http', '$location'
 
                     if(typeof response[index].schoolName !== "undefined")
                     {
+                        $scope.total_school += response[index].schoolName;
+
                         temp = {
                             color: colors[color_number],
                             name: response[index].schoolName,
@@ -117,6 +144,7 @@ app.controller('ReportController', ['$rootScope', '$scope', '$http', '$location'
             .success(function (response) {
 
                 var temp_ethnicity = [];
+                $scope.total_student = 0;
 
                 for (var index in response)
                 {
@@ -124,6 +152,7 @@ app.controller('ReportController', ['$rootScope', '$scope', '$http', '$location'
                     var temp;
                     if(typeof response[index].ethnicity !== "undefined")
                     {
+                        $scope.total_student += response[index].total;
                         temp = {
                             color: colors[color_number],
                             name: response[index].ethnicity,
