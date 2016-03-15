2522412343ae10e623f1ee097d448007b1ad7eb5
ridwansidik85 <ridwan@upwardstech.com>
2016-03-15 18:53:44 +0700

    add multiple email in student detail

--- src/public/asset/services/services.js
+++ src/public/asset/services/services.js
@@ -352,17 +352,29 @@ app.factory('httpInterceptor', function ($q,$rollbar,$location) {
                 //if(!data)
                 //    return $q.reject(response);
             }
+
             return response;
         },
         responseError: function (response) {
+
             if(response.status === 500 || response.status === 502){
-                $rollbar.error(response);
-                showError(response.statusText, 1);
+                if(response.status === 500 && response.data.error_description !=="Password did not match"){
+                    $rollbar.error(response);
+                    showError(response.statusText, 1);
+                }else if(response.status === 502){
+                    $rollbar.error(response);
+                    showError(response.statusText, 1);
+                }
+
             }else if(response.status === 401)
             {
-                showError(response.statusText, 1);
-                $location.path('/login');
-                $rollbar.error(response);
+
+                if(response.data !== 'Unauthorized'){
+                    showError(response.statusText, 1);
+                    $location.path('/login');
+                    $rollbar.error(response);
+                    console.log("rollbar was called");
+                }
             }
             return $q.reject(response);
         }
